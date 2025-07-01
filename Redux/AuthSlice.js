6d24a2474b95f71a5e9c0b9c218c55dcdaecd673
Aryan Raj <raj230439@gmail.com>
2025-07-01 18:18:46 +0530

    new login functionality implemented

--- Redux/AuthSlice.js
+++ Redux/AuthSlice.js
@@ -1,17 +1,12 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axiosInstance from '../src/Helper/axiosInstance.js'
-import toast from "react-hot-toast";
+import {toast} from "react-hot-toast";
 const initialState = {
   isLoggedIn: localStorage.getItem("isLoggedIn") || false,
   role: localStorage.getItem("role") || "",
   data: localStorage.getItem("data") || {},
 };
 
-const authSlice = createSlice({
-  name: "auth",
-  initialState,
-  reducers: {},
-});
 export const createAccount = createAsyncThunk("/auth/signup", async (data) => {
   try {
     const res = axiosInstance.post("user/register", data);
@@ -26,6 +21,28 @@ export const createAccount = createAsyncThunk("/auth/signup", async (data) => {
   } catch (error) {
     toast.error(error?.response?.data?.message);
   }
+});
+export const login = createAsyncThunk("/auth/login", async (data) => {
+  try {
+    const res = axiosInstance.post("user/login", data);
+    toast.promise(res, {
+      loading: "Wait! authentication under process",
+      success: (data) => {
+        return data?.data?.message;
+      },
+      error: "Failed to login",
+    });
+    return (await res).data;
+  } catch (error) {
+    toast.error(error?.response?.data?.message);
+  }
+});
+
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {},
 });
 
 // export const {} = authSlice.actions;
--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import HomePage from './Pages/HomePage';
-import { Route,Routes } from 'react-router-dom'
-import AboutUs from './Pages/AboutUs';
-import NotFound from './Pages/NotFound';
-import Positioning from './Pages/Positioning';
-import SignUp from './Pages/SignUp';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./Pages/HomePage.jsx";
+import AboutUs from "./Pages/AboutUs.jsx";
+import SignUp from "./Pages/SignUp.jsx";
+import NotFound from "./Pages/NotFound.jsx";
+import Login from "./Pages/Login.jsx";
+
 const App = () => {
   return (
-    <>
-     <Routes>
-        <Route path="/" element={<HomePage />} ></Route>
-        <Route path='/about' element={<AboutUs/>}> </Route>
-        <Route path='*' element={<NotFound/>}></Route>
-        <Route path='/position' element={<Positioning/>}></Route>
-        <Route path='/signup' element={<SignUp/>}></Route>
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/about" element={<AboutUs />}></Route>
+      <Route path="/signup" element={<SignUp />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="*" element={<NotFound />}></Route>
     </Routes>
-    </>
-  )
-}
-export default App
+  );
+};
+
+export default App;
